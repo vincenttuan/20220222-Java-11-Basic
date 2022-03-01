0059beb089a3c29ccb047c1d49d1d1585ca9b45f
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-03-01 21:32:48 +0800

    Add files via upload

---
+++ javacode/day3/BitExpression2.java
@@ -0,0 +1,26 @@
+package day3;
+
+public class BitExpression2 {
+	public static void main(String[] args) {
+		int slot = 0b10110;
+		System.out.print(slot);
+		System.out.print(" ==> ");
+		System.out.println(Integer.toBinaryString(slot));
+		int n = Integer.parseInt(args[0]);
+		int park = (int)Math.pow(2, n);
+		System.out.print(park);
+		System.out.print(" ==> ");
+		System.out.println(Integer.toBinaryString(park));
+		int check = slot & park;
+		if(check > 0) {
+			System.out.println("Thanks a lot!");
+			slot = slot - park;
+			System.out.print(slot);
+			System.out.print(" ==> ");
+			System.out.println(Integer.toBinaryString(slot));
+		} else {
+			System.out.println("No Car");
+		}
+
+	}
+}
