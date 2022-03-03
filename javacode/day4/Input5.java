a56ba731aab2d625da6aa632add3d55a79c220ac
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-03-03 20:47:31 +0800

    Add files via upload

--- javacode/day4/Input5.java
+++ javacode/day4/Input5.java
@@ -17,13 +17,56 @@ public class Input5 {
 		char area = id.charAt(2);
 		System.out.printf("area = %c\n", area);
 
-		// 判斷男女
+		// if-else 判斷男女
 		if(sex == '1') {
 			System.out.println("男性");
 		} else if(sex == '2') {
 			System.out.println("女性");
 		} else {
 			System.out.println("性別錯誤");
+		}
+
+		// switch-case 判斷男女
+		switch(sex) {
+			case '1':
+				System.out.println("男性");
+				break;
+			case '2':
+				System.out.println("女性");
+				break;
+			default:
+				System.out.println("性別錯誤");			
+		}
+
+		// switcg-case 判斷第三碼 area
+		// 0~5  其他
+		// 6    取得國籍之外國人
+		// 7    無戶籍國民
+		// 8    港澳居民
+		// 9    大陸地區人民
+		switch(area) {
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+				System.out.println("其他");
+				break;
+			case '6':
+				System.out.println("取得國籍之外國人");
+				break;	
+			case '7':
+				System.out.println("無戶籍國民");
+				break;	
+			case '8':
+				System.out.println("港澳居民");
+				break;	
+			case '9':
+				System.out.println("大陸地區人民");
+				break;
+			default:
+				System.out.println("area 錯誤");	
 		}
 
 	}
