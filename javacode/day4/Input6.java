201326b0d3c6bb7a782a2f6504b1330baf4a75a1
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-03-03 20:53:59 +0800

    Add files via upload

---
+++ javacode/day4/Input6.java
@@ -0,0 +1,21 @@
+package day4;
+import java.util.Scanner;
+
+public class Input6 {
+	public static void main(String[] args) {
+		// input score and 
+		// print level:
+		// 90~100: A
+		// 80~<90: B
+		// 70~<80: C
+		// 60~<70: D
+		// 0~<60 : E
+		// only use switch-case
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input score = ");
+		int score = sc.nextInt();
+
+		
+		
+	}
+}
